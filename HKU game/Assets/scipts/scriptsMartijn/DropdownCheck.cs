aaee80434f95910684abe0dea3ebea80af341001
Murmur76 <martijnlaureijs@gmail.com>
2024-10-22 12:14:31 +0200

    changed the dropdown

--- HKU game/Assets/scipts/scriptsMartijn/DropdownCheck.cs
+++ HKU game/Assets/scipts/scriptsMartijn/DropdownCheck.cs
@@ -18,8 +18,30 @@ public class DropdownCheck : MonoBehaviour
         int pickedEntryIndexPoint = dropdownPoint.value;
         int pickedEntryIndexDirec = dropdownDirec.value;
 
-        
-
+        if (pickedEntryIndexPoint == 0)
+        {
+            Grandma.upDirection = true;
+        }
+        if (pickedEntryIndexPoint == 1)
+        {
+            Grandma.downDirection = true;
+        }
+        if (pickedEntryIndexPoint == 2)
+        {
+            Grandma.leftDirection = true;
+        }
+        if (pickedEntryIndexPoint == 3)
+        {
+            Grandma.rightDirection = true;
+        }
+        if (pickedEntryIndexDirec == 0)
+        {
+            Grandma.leftDirection = true;
+        }
+        if (pickedEntryIndexDirec == 1)
+        {
+            Grandma.rightDirection = true;
+        }
     }
 
 
